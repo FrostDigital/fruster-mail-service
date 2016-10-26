818d844446e0f42e530ccdaaf1c8dffad38c7f66
Florry <viktor@frostdigital.se>
2016-10-26 17:12:07 +0200

    cleans

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const mailService = require('./mail-service');
 const conf = require('./conf');
 
 mailService
-  .start(conf.bus)
-  .then(() => {
-    log.info('Mail service started and connected to bus', conf.bus);
-  })
-  .error(err => {
-    log.error('Failed starting mail service', err);
-    process.exit(1);
-  });
-
+	.start(conf.bus)
+	.then(() => {
+		log.info('Mail service started and connected to bus', conf.bus);
+	})
+	.catch(err => {
+		log.error('Failed starting mail service', err);
+		process.exit(1);
+	});
--- mail-service.js
+++ mail-service.js
@@ -2,7 +2,8 @@ const bus = require('fruster-bus');
 const sendMail = require('./lib/send-mail');
 
 module.exports.start = function (busAddress)  {
-  return bus.connect(busAddress).then(() =>  {    
-    bus.subscribe('mail-service.send', req => sendMail(req));
-  });    
+	return bus.connect(busAddress)
+		.then(() =>  {
+			bus.subscribe('mail-service.send', req => sendMail(req));
+		});
 };
