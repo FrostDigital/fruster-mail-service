3c4fc3f990ba884e65d5bcbd65ec0fe0d9a66095
Joel Soderstrom <joel.soderstrom@gmail.com>
2017-01-02 23:18:41 +0100

    Adds fruster health and upgrades deps

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const log = require('fruster-log');
 const mailService = require('./mail-service');
 const conf = require('./conf');
 
+require("fruster-health").start();
+
 mailService
 	.start(conf.bus)
 	.then(() => {
