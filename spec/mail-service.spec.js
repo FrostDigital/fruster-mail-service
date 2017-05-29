ef4adb5e7e4c7d071a412623e873bdfcd2dca2c7
Joel Soderstrom <joel.soderstrom@gmail.com>
2017-05-29 10:54:34 +0200

    Implements use of templates

--- spec/mail-service.spec.js
+++ spec/mail-service.spec.js
@@ -1,15 +1,15 @@
-var log = require('fruster-log'),      
-    bus = require('fruster-bus'),
-    nsc = require('nats-server-control'),
-    mailService = require('../mail-service');
+const log = require("fruster-log");
+const bus = require("fruster-bus");
+const nsc = require("nats-server-control");
+const mailService = require("../mail-service");
 
-describe('Mail service', () => {
+describe("Mail service", () => {
   
   var natsServer, busPort, busAddress;    
-
+  
   beforeEach(done => {
     busPort = Math.floor(Math.random() * 6000 + 2000);
-    busAddress = 'nats://localhost:' + busPort;
+    busAddress = "nats://localhost:" + busPort;
 
     nsc.startServer(busPort)
       .then(server => { natsServer = server; })
@@ -25,15 +25,36 @@ describe('Mail service', () => {
   });
 
   // Note: Think about disabling this test
-  it('should send mail for real', done => {              
+  it("should send mail for real", done => {              
     var mail = {
-      to: 'joel@frost.se',
-      from: 'fruster@frost.se',
-      subject: 'Hello world from automated test',
-      message: 'This is a message from me in the future: This is not a good idea.'
+      to: "joel@frost.se",
+      from: "fruster@frost.se",
+      subject: "Hello world from automated test",
+      message: "This is a message from me in the future: This is not a good idea."
     };
 
-    bus.request('mail-service.send', { data: mail })
+    bus.request("mail-service.send", { data: mail })
+    .then(resp => {
+      expect(resp.status).toBe(200);
+      done();
+    })
+    .catch(done.fail);
+    
+  });
+
+  it("should send mail and use a template", done => {              
+    var mail = {
+      to: "joel@frost.se",
+      from: "fruster@frost.se",
+      subject: "Hello world from template test",
+      message: "This is the message body.",
+      templateId: "fc27a67e-b59b-4dc1-bfaa-ee3d9804e1a5",
+      templateArgs: {
+        name: "Joel"
+      }
+    };
+
+    bus.request("mail-service.send", { data: mail })
     .then(resp => {
       expect(resp.status).toBe(200);
       done();
