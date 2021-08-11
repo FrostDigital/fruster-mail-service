e5b7b5b80fab0abd03a16e64db8ae7d42f426ae0
Dinuka Thilanga <dinuka@frost.se>
2021-08-11 09:33:06 +0530

    [DEV] Remove unwanted line in schemas

--- lib/schemas/ISendMailRequestSchemas.ts
+++ lib/schemas/ISendMailRequestSchemas.ts
@@ -5,7 +5,5 @@ import Mail from "../models/Mail";
  * Can be used in two ways: mail contents defined in request or with template.
  * If contents is in request; `to`, `subject` and `message` are required.
  * If template is used; `to` and `templateId` are required.
- *
- * @additionalProperties false
  */
 export default interface SendMailRequest extends Mail { };
